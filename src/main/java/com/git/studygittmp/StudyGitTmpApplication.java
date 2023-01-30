ff91571e565aa3e955ff52f76d8c2348de1a2677
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-01-30 21:16:42 +0900

    initial setting

---
+++ src/main/java/com/git/studygittmp/StudyGitTmpApplication.java
@@ -0,0 +1,13 @@
+package com.git.studygittmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGitTmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGitTmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/git/studygittmp/StudyGitTmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.studygittmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGitTmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
